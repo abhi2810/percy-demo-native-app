a4818af27b7bc4d4d6ae7464a04e30bb690ffa03
Abhi Singh <abhi.s@browserstack.com>
2021-05-25 10:23:37 +0530

    added sample app and web view

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, BackHandler, Platform, } from 'react-native';
+import { WebView } from 'react-native-webview';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  webView = {
+    canGoBack: false,
+    ref: null,
+  }
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  onAndroidBackPress = () => {
+    if (this.webView.canGoBack && this.webView.ref) {
+      this.webView.ref.goBack();
+      return true;
+    }
+    return false;
+  }
+
+  componentWillMount() {
+    if (Platform.OS === 'android') {
+      BackHandler.addEventListener('hardwareBackPress', this.onAndroidBackPress);
+    }
+  }
+
+  componentWillUnmount() {
+    if (Platform.OS === 'android') {
+      BackHandler.removeEventListener('hardwareBackPress');
+    }
+  }
+
+  render() {
+    if (Platform.OS === 'android') {
+      return (
+        <View style={{ flex: 1 }}>
+          <WebView
+            ref={(webView) => { this.webView.ref = webView; }}
+            onNavigationStateChange={(navState) => { this.webView.canGoBack = navState.canGoBack; }}
+            originWhitelist={['*']}
+            automaticallyAdjustContentInsets={false}
+            source={{ uri: 'file:///android_asset/index.html' }}
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            startInLoadingState={true}
+          />
+        </View>
+      )
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <WebView
+            ref={(webView) => { this.webView.ref = webView; }}
+            onNavigationStateChange={(navState) => { this.webView.canGoBack = navState.canGoBack; }}
+            originWhitelist={['*']}
+            automaticallyAdjustContentInsets={false}
+            source={{ uri: 'Web.bundle/index.html' }}
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            startInLoadingState={true}
+          />
+        </View>
+      )
+    }
+  }
+
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
